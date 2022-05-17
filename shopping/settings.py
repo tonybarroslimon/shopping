13955803322783f1e8765bd88d2e26ec9ba98390
tonybarroslimon <tonybarroslimon@outlook.com>
2022-05-17 11:36:50 -0700

    more updates to upgrade to Django 4.0.2

--- shopping/settings.py
+++ shopping/settings.py
@@ -119,7 +119,7 @@ INSTALLED_APPS = [
     # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
     'django.contrib.flatpages',
-    # 'registration',
+    'registration',
     'products',
     'contact',
     'cart',
