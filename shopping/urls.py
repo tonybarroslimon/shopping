b464ddb11ee88f7602ea173a33ddc7624e76a9bb
tonybarroslimon <tonybarroslimon@outlook.com>
2022-05-19 11:44:23 -0700

    upgraded the urls.py file to include the new 'path' upgrade from the old url syntax. More work to update from django 1 to 4.

--- shopping/urls.py
+++ shopping/urls.py
@@ -1,23 +1,29 @@
 import os
-from django.conf.urls import patterns, include, url
+import products.views
+import cart.views
+import checkout.views
+import contact.views
+import customers.views
+from django.contrib import admin, admindocs
+from django.urls import path, re_path
+from django.conf.urls import include
 
-from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    url(r'^admin/', include(admin.site.urls)),
-    (r'^accounts/', include('registration.backends.default.urls')),
-    (r'^media(?P<path>, *)$', 'django.views.static.serve', {'document_root':os.path.join(os.path.dirname(__file__), 'static/media/')}),
-    (r'^static(?P<path>, *)$', 'django.views.static.serve', {'document_root':os.path.join(os.path.dirname(__file__), 'static')}),
-    url(r'^$', 'products.views.home'),
-    (r'^pages/', include('django.contrib.flatpages.urls')),
-    url(r'^products/$', 'products.views.products'),
-    url(r'^products/(?P<slug>[-\w]+)/$', 'products.views.product_single'),
-    #url(r'^products/(?P<slug>[-\w]+)/add$', 'cart.views.add'),
-    url(r'^contact/$', 'contact.views.contact'),
-    url(r'^cart/$', 'cart.views.view'),
-    url(r'^cart/add', 'cart.views.add_to_cart'),
-    url(r'^cart/delete$', 'cart.views.delete'),
-    url(r'^checkout/', 'checkout.views.checkout'),
-)
+urlpatterns = [
+    path('admin/doc/', include('django.contrib.admindocs.urls')),
+    path('admin/', admin.site.urls),
+    #re_path(r'^accounts/', registration.backends.default.urls),
+    #re_path(r'^media(?P<path>, *)$', 'django.views.static.serve', {'document_root':os.path.join(os.path.dirname(__file__), 'static/media/')}),
+    #re_path(r'^static(?P<path>, *)$', 'django.views.static.serve', {'document_root':os.path.join(os.path.dirname(__file__), 'static')}),
+    re_path(r'^$', products.views.home),
+    re_path(r'^pages/', django.contrib.flatpages.urls),
+    re_path(r'^products/$', products.views.products),
+    re_path(r'^products/(?P<slug>[-\w]+)/$', products.views.product_single),
+    #re_path(r'^products/(?P<slug>[-\w]+)/add$', cart.views.add),
+    re_path(r'^contact/$', contact.views.contact),
+    re_path(r'^cart/$', cart.views.view),
+    re_path(r'^cart/add', cart.views.add_to_cart),
+    re_path(r'^cart/delete$', cart.views.delete),
+    re_path(r'^checkout/', checkout.views.checkout),
+]
